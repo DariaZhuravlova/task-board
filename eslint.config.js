ff89e8e7b3e347e7e4af6f659cc865e43fe94524
Dimatuzkoff <dimatuzkoff@gmail.com>
2026-04-18 15:33:15 +0300

    changed type of quotes

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,16 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
-import { Quote } from 'lucide-react'
-import react from 'eslint-plugin-react'
+import js from "@eslint/js"
+import globals from "globals"
+import reactHooks from "eslint-plugin-react-hooks"
+import reactRefresh from "eslint-plugin-react-refresh"
+import tseslint from "typescript-eslint"
+import { defineConfig, globalIgnores } from "eslint/config"
+import { Quote } from "lucide-react"
+import react from "eslint-plugin-react"
 
 export default defineConfig([
-    globalIgnores(['dist']),
+    globalIgnores(["dist"]),
     {
-        files: ['**/*.{ts,tsx}'],
+        files: ["**/*.{ts,tsx}"],
         plugins: { react },
         extends: [
             js.configs.recommended,
@@ -19,23 +19,23 @@ export default defineConfig([
             reactRefresh.configs.vite,
         ],
         rules: {
-            quotes: ['warn', 'single'],
-            indent: ['warn', 4],
-            'comma-dangle': ['error', 'never'],
-            semi: ['warn', 'never'],
-            'max-lines': ['error', {
+            quotes: ["warn", "double"],
+            indent: ["warn", 4],
+            "comma-dangle": ["error", "never"],
+            semi: ["warn", "never"],
+            "max-lines": ["error", {
                 max: 200,
                 skipBlankLines: true,
                 skipComments: true,
             }],
-            'react-hooks/rules-of-hooks': 'error',
-            'react-hooks/exhaustive-deps': 'warn',
-            'object-curly-spacing': ['error', 'always'],
-            'react/jsx-curly-spacing': ['error', { 'when': 'always', 'children': true }],
-            'react/jsx-key': 'error',
-            'react/no-unused-prop-types': 'warn',
-            'react/jsx-no-useless-fragment': 'warn',
-            '@typescript-eslint/consistent-type-imports': ['warn', { prefer: 'type-imports' }]
+            "react-hooks/rules-of-hooks": "error",
+            "react-hooks/exhaustive-deps": "warn",
+            "object-curly-spacing": ["error", "always"],
+            "react/jsx-curly-spacing": ["error", { "when": "always", "children": true }],
+            "react/jsx-key": "error",
+            "react/no-unused-prop-types": "warn",
+            "react/jsx-no-useless-fragment": "warn",
+            "@typescript-eslint/consistent-type-imports": ["warn", { prefer: "type-imports" }]
         },
         languageOptions: {
             ecmaVersion: 2020,
